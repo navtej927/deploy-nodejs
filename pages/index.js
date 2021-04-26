d752e9670d1424e8c30cfc68f6c1d3ea93d5e3c2
navtej927 <navtej927@gmail.com>
2021-04-25 21:10:27 -0700

    nextjs

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function HomePage() {
+    return <div>
+        <p>This is my application v2</p>
+    </div >
+}
+
+export default HomePage
---
+++ server.js
@@ -0,0 +1,42 @@
+const next = require('next');
+const express = require("express");
+
+const port = process.env.PORT || 3000
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const app = next({ isDevelopment });
+const handle = app.getRequestHandler();
+
+
+app.prepare().then(() => {
+    const server = express()
+
+    server.get('/api/news', async (req, res) => {
+        const response = await axios.get("http://localhost:3008/api/v1/news");
+        console.log("response is", response.data);
+        res.send(response.data);
+    });
+
+    server.get('/api/static', async (req, res) => {
+        const response = await axios.get("http://localhost:3008/api/v1/static");
+        res.send(response.data);
+    });
+
+    server.get('/api/ping', async (req, res) => {
+        res.send({
+            1: 1
+        });
+    });
+
+    server.all('*', (req, res) => {
+        return handle(req, res)
+    })
+
+    server.listen(port, (err) => {
+        if (err) throw err
+        console.log(`> Ready on http://localhost:${port}`)
+    })
+
+}).catch(err => {
+    console.log('Error:::::', err)
+})
